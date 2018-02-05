53e9a24f05659d3e168c48a64aca97948335590e
criscompbr <criscompbr@gmail.com>
2018-02-05 14:45:42 -0200

    Primeiro projeto

--- index.php
+++ index.php
@@ -7,105 +7,106 @@ require_once "class/apiCliente.php";
 
 $c = new cliente();
 
+$utilize = 'Utilize o exemplo do url http://' . $_SERVER['HTTP_HOST'] . '/?r=getCupons';
 if(!isset($_GET['r'])){
-  echo ".....";
+  echo $utilize;
 }else{
   echo "<pre>";
   switch ($_GET['r']) {
     
-    // Cupons [GET: OK, PUT: OK]
+    // Cupons
     case 'getCupons':
       var_dump($c->API_Download('GET', 'cupons'));
       break;
     case 'putCupons':
       var_dump($c->API_Download('PUT', 'cupons', [
-        'cupom' => 18011404292
+        'cupom' => 123456
       ]));
       break;
     
-    // Franqueados [GET: OK, PUT: OK]
+    // Franqueados
     case 'getFranqueados':
       var_dump($c->API_Download('GET', 'franqueados'));
       break;
     case 'putFranqueados':
       var_dump($c->API_Download('PUT', 'franqueados', [
-        'id' => 318,
+        'id' => 145,
         'ativo' => 1
       ]));
       break;
     
-    // Premios [GET: OK, PUT: OK, DELETE: OK]
+    // Premios
     case 'getPremios':
       var_dump($c->API_Download('GET', 'premios'));
       break;
     case 'putPremios':
       var_dump($c->API_Download('PUT', 'premios', [
-        'id' => 1078,
-        'estoque' => 700
+        'id' => 145,
+        'estoque' => 1
       ]));
       break;
     case 'deletePremios':
       var_dump($c->API_Download('DELETE', 'premios', [
-        'id' => 1078
+        'id' => 145
       ]));
       break;
 
-    // Categorias [GET: OK, POST: OK, PUT: OK, DELETE: OK]
+    // Categorias
     case 'getCategorias':
       var_dump($c->API_Download('GET', 'categorias'));
       break;
     case 'postCategorias':
       var_dump($c->API_Download('POST', 'categorias', [
-        'descricao' => 'PHP'
+        'descricao' => ''
       ]));
       break;
     case 'putCategorias':
       var_dump($c->API_Download('PUT', 'categorias', [
-        'id' => 2814,
-        'descricao' => 'PHP 7.0'
+        'id' => 145,
+        'descricao' => ''
       ]));
       break;
     case 'deleteCategorias':
       var_dump($c->API_Download('DELETE', 'categorias', [
-        'id' => 2815,
+        'id' => 145,
       ]));
       break;
 
-    // Usuários [GET: OK, POST: OK, PUT: OK]
+    // Usuários
     case 'getUsuarios':
       var_dump($c->API_Download('GET', 'usuarios'));
       break;
     case 'postUsuarios':
       var_dump($c->API_Download('POST', 'usuarios', [
-        'nome' => 'Cliente API #3',
-        'cpf' => 13167260025,
-        'email' => 'cliente8@gmail.com',
-        'telefone' => 5137141811,
+        'nome' => 'Cliente',
+        'cpf' => '',
+        'email' => 'cliente@gmail.com',
+        'telefone' => 5112345678,
         'senha' => 12345
       ]));
       break;
     case 'putUsuarios':
       var_dump($c->API_Download('PUT', 'usuarios', [
-        'cpf' => '693.186.500-51',
+        'cpf' => '',
         'ativo' => 1
       ]));
       break;
  
-    // Cupons [GET: OK, POST: OK]
+    // Cupons
     case 'getPontos':
       var_dump($c->API_Download('GET', 'pontos'));
       break;
     case 'postPontos':
       var_dump($c->API_Download('POST', 'pontos', [
-        'cpf' => 95662223009,
-        'quantidade' => -1,
-        'observacao' => "Devolve o 88 pq o PDF eh falso!!"
+        'cpf' => '',
+        'quantidade' => 1,
+        'observacao' => ''
       ]));
       break;
 
     // Retorna link invalido
     default:
-      echo "....";
+      echo $utilize;
 
   }
 }
